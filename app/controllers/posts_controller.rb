bad0dd0ac36a0382ca5f9ed8499dfa8f255549db
HOME <hhw1990@naver.com>
2018-06-25 16:35:18 +0900

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  #로그인해야 글쓰기가 됨
+  before_action :authenticate_user!, except: :index
+
   def index
     @posts = Post.all
   end
